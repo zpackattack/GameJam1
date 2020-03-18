1b2ffaa73e06a7564f218be2e1577d439ee37fc5
Yegor <yehor.tverdokhlibov@gmail.com>
2020-03-18 17:24:55 +0200

    Bacteria 1.0
    Added:
    Movement
    Collision
    Enemies

---
+++ GameJam1/Assets/EnemyControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControl : MonoBehaviour
+{
+
+    float directionX;
+    float directionY;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        directionX = Random.Range(-5,5);
+        directionY = Random.Range(-5, 5);
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Random.Range(-10,10) > 6)
+        {
+            directionX = Random.Range(-5, 5);
+            directionY = Random.Range(-5, 5);
+        }
+
+        //if (Random.Range(0,2) > 1)
+            rb.AddForce(directionX, directionY, 0);
+    }
+}
---
+++ GameJam1/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float Speed = 0.1f;
+    Rigidbody rb;
+    public float currentSize = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(-Speed, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(0, Speed, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Speed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(0, -Speed, 0);
+        }        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("fired");
+        if (collision.gameObject.name == "Enemy")
+        {
+            Destroy(collision.gameObject);
+            currentSize += 0.5f;
+            transform.localScale = new Vector3(currentSize, currentSize, 1);
+        }
+
+    }
+}
+
